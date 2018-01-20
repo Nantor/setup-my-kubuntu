86a0da4be49fa76489d805dac9f2c6446efb79a0
Nantor <SpamMail001@gmx.de>
2018-01-20 02:06:27 +0100

    add docker compose 1.18.0

--- install_general.bash
+++ install_general.bash
@@ -10,6 +10,8 @@ sudo apt -y install git gcc perl make chromium-browser firefox curl gimp apt-tra
 ## install docker
 curl -fsSL get.docker.com | sudo sh
 sudo usermod -aG docker $(whoami)
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 ## install nodejs
 wget -qO- https://deb.nodesource.com/setup_9.x | sudo -E bash -
