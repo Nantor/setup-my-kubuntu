65d707012fcd446afdad9c54fd391fbb34100fb1
Nantor <SpamMail001@gmx.de>
2018-01-26 17:25:27 +0100

    refactoring

--- install_general.bash
+++ install_general.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## temp-file for downloading packs
+vsc_tmp=$(mktemp)
+
 ## update system
 sudo apt update
 sudo apt -y upgrade
@@ -7,7 +10,7 @@ sudo apt -y upgrade
 ## install packs
 sudo apt -y install git gcc perl make chromium-browser firefox curl gimp apt-transport-https ca-certificates software-properties-common filezilla
 
-## install docker
+## install docker and docker-compose
 curl -fsSL get.docker.com | sudo sh
 sudo usermod -aG docker $(whoami)
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
@@ -23,7 +26,6 @@ echo "export PATH=~/.npm-global/bin:\$PATH" > ~/.profile
 source ~/.profile
 
 ## install Visual Studio Code
-vsc_tmp=$(mktemp)
 wget -O $vsc_tmp https://go.microsoft.com/fwlink/?LinkID=760868
 sudo dpkg -i $vsc_tmp
 sudo apt-get install -f
