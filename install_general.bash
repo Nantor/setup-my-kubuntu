ef869d33432831f2be6da6f5e6b80ee00ac8572f
Nantor <SpamMail001@gmx.de>
2019-02-19 11:08:32 +0100

    install chromium and firefox over apt

--- install_general.bash
+++ install_general.bash
@@ -5,14 +5,12 @@ sudo apt update
 sudo apt -y upgrade
 
 ## install packs
-sudo apt -y install git gcc perl make gimp apt-transport-https ca-certificates software-properties-common filezilla openjdk-11-jdk openjdk-11-source update-manager snap python3-distutils
+sudo apt -y install git gcc perl make gimp apt-transport-https ca-certificates software-properties-common filezilla openjdk-11-jdk openjdk-11-source update-manager snap python3-distutils firefox chromium
 
 ## install nodejs
 curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 
 ## snap install apps
-sudo snap install chromium
-sudo snap install firefox
 sudo snap install pycharm-community --classic
 sudo snap install intellij-idea-ultimate --classic
 sudo snap install vscode --classic
