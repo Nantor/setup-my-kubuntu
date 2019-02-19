b6172a72ecad892b1810d114a64ed6f3f54448df
Nantor <SpamMail001@gmx.de>
2019-02-19 10:50:03 +0100

    use batch script for nodejs/npm

--- install_general.bash
+++ install_general.bash
@@ -7,10 +7,12 @@ sudo apt -y upgrade
 ## install packs
 sudo apt -y install git gcc perl make gimp apt-transport-https ca-certificates software-properties-common filezilla openjdk-11-jdk openjdk-11-source update-manager snap python3-distutils
 
-## snap install
+## install nodejs
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+
+## snap install apps
 sudo snap install chromium
 sudo snap install firefox
-sudo snap install node --channel=11/stable --classic
 sudo snap install pycharm-community --classic
 sudo snap install intellij-idea-ultimate --classic
 sudo snap install vscode --classic
