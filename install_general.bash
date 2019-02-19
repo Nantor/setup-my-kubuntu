41e52f67f23ac998e7640f6d3fcce0d9de580724
Nantor <SpamMail001@gmx.de>
2019-02-19 11:25:22 +0100

    add yarn

--- install_general.bash
+++ install_general.bash
@@ -5,10 +5,17 @@ sudo apt update
 sudo apt -y upgrade
 
 ## install packs
-sudo apt -y install git gcc perl make gimp apt-transport-https ca-certificates software-properties-common filezilla openjdk-11-jdk openjdk-11-source update-manager snap python3-distutils firefox curl
+sudo apt -y install git gcc g++ perl make gimp apt-transport-https ca-certificates software-properties-common filezilla openjdk-11-jdk openjdk-11-source update-manager snap python3-distutils firefox curl
 
 ## install nodejs
 curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+## install yarn
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
 
 ## snap install apps
 sudo snap install chromium --classic
