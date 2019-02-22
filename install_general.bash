b5f2c9de4d0d578d36176c41b50ba255ba5fd090
Nantor <SpamMail001@gmx.de>
2019-02-22 18:25:44 +0100

    rearrange code

--- install_general.bash
+++ install_general.bash
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-## update system
-sudo apt update
-sudo apt -y upgrade
-
-## install packs
-sudo apt -y install git gcc g++ perl make gimp apt-transport-https ca-certificates software-properties-common filezilla openjdk-11-jdk openjdk-11-source update-manager snap python3-distutils firefox curl
-
 ## install nodejs
 curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-sudo apt-get install -y nodejs
 
 ## install yarn
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+
+## install packs
+sudo apt update
+sudo apt -y install git gcc g++ perl make gimp apt-transport-https ca-certificates software-properties-common filezilla openjdk-11-jdk openjdk-11-source update-manager snap python3-distutils firefox yarn nodejs
 
 ## snap install apps
 sudo snap install chromium --classic
@@ -23,8 +18,10 @@ sudo snap install intellij-idea-ultimate --classic
 sudo snap install vscode --classic
 sudo snap install docker
 
-## clean up
+## update system
 sudo apt update
 sudo apt -y upgrade
+
+## clean up
 sudo apt -y autoremove
 sudo apt -y autoclean
